653e77b82900d6dd60321e97759c13747a975178
jolah1 <jokunlade@gmail.com>
2022-05-19 06:44:01 -0700

    updating files

--- get_flag.c
+++ get_flag.c
@@ -4,29 +4,30 @@
  * get_flag - turns on flags if _printf finds
  * @s: character that hods the flag specifier
  * @f: pointer to the struct flags in which we turn the flags on
- * Return: 1 if a flag has been turned on, 0 otherwise
+ * Return: 1 if a flag has been turned on, 0 if not
  */
 
 int get_flag(char s, flags_t *f)
 {
 
-	int i = 0;
+	int x = 0;
 
 	switch (s)
 	{
 		case '+':
 			f->plus = 1;
-			i = 1;
+			x = 1;
 			break;
 		case ' ':
 			f->space = 1;
-			i = 1;
+			x = 1;
 			break;
 		case '#':
 			f->hash = 1;
-			i = 1;
+			x = 1;
 			break;
 	}
 
-	return (i);
+
+	return (x);
 }
