e6e3d4bb40cd209a691da8d7e161f4a8fea7626b
Ayinla Abel <kraneabel@gmail.com>
2022-05-19 03:59:30 -0700

    main.h updated

--- main.h
+++ main.h
@@ -1,9 +1,66 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct flags - struct containing flags to turn on
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+
+stypedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function
+ * @c: format specifier passed to _print()
+ * @f: point to the correct print function
+ */
+
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+
+char *convert(unsigned long int num, int base, int lowercase);
 
 int _printf(const char *format, ...);
+
+int (*get_print(char s))(va_list, flags_t *);
+
+int get_flag(char s, flags_t *f);
+
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+
+int _putchar(char c);
+int _puts(char *str);
+
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+
+int print_address(va_list l, flags_t *f);
+
+int print_percent(va_list l, flags_t *f);
+
 
 #endif
